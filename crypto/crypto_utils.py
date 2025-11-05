c96fb39d1e6a859b97e967bbe252b631ad5bc3e8
Isabela Rangel <isabelarangel2008@gmail.com>
2025-11-05 19:45:55 -0300

    deixei bonitinho

--- crypto/crypto_utils.py
+++ crypto/crypto_utils.py
@@ -3,6 +3,7 @@ from cryptography.fernet import Fernet
 chave = b'2O1j6m8lFh1r2q9P3tLZQx7nT1mFv5yVx7J8X2nM3R4='  
 fernet = Fernet(chave)
 
+
 def criptografar(texto):
     return fernet.encrypt(texto.encode())
 
